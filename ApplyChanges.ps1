44e8aab86e0e6b6619a194a5344a47cbf4a8be5b
Vyacheslav Skvortsov <vyacheslav.skvortsov@abbyy.com>
2017-09-04 09:00:55 +0300

    Добавил подсветку карточек, фильтры, новые статусы

---
+++ ApplyChanges.ps1
@@ -0,0 +1,19 @@
+﻿$witadmin = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\witadmin.exe"
+
+cd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\"
+ 
+ 
+& $witadmin importwitd /collection:http://skvortsovv:8080/tfs/DefaultCollection /p:FROL_Agile /f:"D:\!tfs\Agile_New\Agile\WorkItem Tracking\TypeDefinitions\Task.xml"
+& $witadmin importwitd /collection:http://skvortsovv:8080/tfs/DefaultCollection /p:FROL_Agile /f:"D:\!tfs\Agile_New\Agile\WorkItem Tracking\TypeDefinitions\Epic.xml"
+& $witadmin importwitd /collection:http://skvortsovv:8080/tfs/DefaultCollection /p:FROL_Agile /f:"D:\!tfs\Agile_New\Agile\WorkItem Tracking\TypeDefinitions\Feature.xml"
+& $witadmin importwitd /collection:http://skvortsovv:8080/tfs/DefaultCollection /p:FROL_Agile /f:"D:\!tfs\Agile_New\Agile\WorkItem Tracking\TypeDefinitions\UserStory.xml"
+& $witadmin importwitd /collection:http://skvortsovv:8080/tfs/DefaultCollection /p:FROL_Agile /f:"D:\!tfs\Agile_New\Agile\WorkItem Tracking\TypeDefinitions\Bug.xml"
+& $witadmin importwitd /collection:http://skvortsovv:8080/tfs/DefaultCollection /p:FROL_Agile /f:"D:\!tfs\Agile_New\Agile\WorkItem Tracking\TypeDefinitions\FeedbackRequest.xml"
+& $witadmin importwitd /collection:http://skvortsovv:8080/tfs/DefaultCollection /p:FROL_Agile /f:"D:\!tfs\Agile_New\Agile\WorkItem Tracking\TypeDefinitions\FeedbackResponse.xml"
+
+
+#& $witadmin exportprocessconfig /collection:http://skvortsovv:8080/tfs/DefaultCollection /p:FROL_Agile /f:"D:\!tfs\ProcessConfig.xml"
+
+#& $witadmin importprocessconfig /collection:http://skvortsovv:8080/tfs/DefaultCollection /p:FROL_Agile /f:"D:\!tfs\ProcessConfig.xml"
+
+& $witadmin importprocessconfig /collection:http://skvortsovv:8080/tfs/DefaultCollection /p:FROL_Agile /f:"D:\!tfs\Agile_New\Agile\WorkItem Tracking\Process\ProcessConfiguration.xml"
